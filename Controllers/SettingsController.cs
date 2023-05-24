a108694cc1d5bca4cca1afd16d4a27befdad46be
Pieter <pieter@L2.co.za>
2023-05-24 09:08:13 +0200

    added effectivate and expire dates to cert output

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
 
 namespace jsdal_server_core.Controllers
 {
@@ -20,13 +21,19 @@ namespace jsdal_server_core.Controllers
                 certStore.Open(OpenFlags.ReadOnly);
 
                 var certList = certStore.Certificates.Cast<X509Certificate2>().ToList();
+
+                CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+
                 var ret = certList.Select(cert => new
                 {
                     cert.HasPrivateKey,
                     FriendlyName = !string.IsNullOrWhiteSpace(cert.FriendlyName) ? cert.FriendlyName : cert.Subject?.Substring(3),
                     cert.Thumbprint,
                     Subject = cert.Subject?.Substring(3),
-                    cert.Issuer
+                    cert.Issuer,
+                    EffectiveDate = DateTime.Parse(cert.GetEffectiveDateString(), CultureInfo.InvariantCulture),
+                    ExpiryDate = DateTime.Parse(cert.GetExpirationDateString(), CultureInfo.InvariantCulture)
                 }).OrderBy(c => c.FriendlyName);
 
                 return ApiResponse.Payload(ret);
@@ -45,12 +52,35 @@ namespace jsdal_server_core.Controllers
         {
             try
             {
+                var certStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+
+                certStore.Open(OpenFlags.ReadOnly);
+
+                var certList = certStore.Certificates.Cast<X509Certificate2>().ToList();
+
                 var ws = Settings.SettingsInstance.Instance.Settings.WebServer;
+
+                                // look for a matching cert from the store
+                var cert = certList.FirstOrDefault(c => c.Thumbprint.Equals(ws.HttpsCertHash, StringComparison.OrdinalIgnoreCase));
+
 
                 return ApiResponse.Payload(new dynamic[]
                 {
                     new { enabled = ws.EnableBasicHttp, hostname = ws.HttpServerHostname, port = ws.HttpServerPort },
-                    new { enabled = ws.EnableSSL, hostname = ws.HttpsServerHostname, port = ws.HttpsServerPort, certThumb = ws.HttpsCertHash }
+                    new {
+                         enabled = ws.EnableSSL, hostname = ws.HttpsServerHostname, port = ws.HttpsServerPort,
+                         cert = new {
+                            cert?.HasPrivateKey,
+                            FriendlyName = !string.IsNullOrWhiteSpace(cert?.FriendlyName) ? cert.FriendlyName : cert.Subject?.Substring(3),
+                            cert?.Thumbprint,
+                            Subject = cert?.Subject?.Substring(3),
+                            cert?.Issuer,
+                            EffectiveDate = cert != null? DateTime.Parse(cert?.GetEffectiveDateString(), CultureInfo.InvariantCulture) : DateTime.MinValue,
+                            ExpiryDate = cert != null?  DateTime.Parse(cert?.GetExpirationDateString(), CultureInfo.InvariantCulture) : DateTime.MinValue
+                         }
+                         //,certThumb = ws.HttpsCertHash
+
+                     }
                 });
             }
             catch (Exception ex)
@@ -76,7 +106,7 @@ namespace jsdal_server_core.Controllers
                 ws.EnableSSL = https.enabled;
                 ws.HttpsServerHostname = https.hostname;
                 ws.HttpsServerPort = https.port;
-                ws.HttpsCertHash = https.certThumb;
+                ws.HttpsCertHash = https.cert?.Thumbprint;
 
                 if (ws.EnableSSL ?? false)
                 {
